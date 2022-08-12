386433f50eba204e71b6a8f03810cb6209f846a8
David D <dadch1404@gmail.com>
2022-08-12 18:04:14 -0500

    feat: first working version

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["content/**/*.md", "themes/tailwind/layouts/**/*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", "sans-serif"],
+      }
+    },
+  },
+  plugins: [],
+};
