c59e90daac74efaad16a4dff007b9e8e3c42731f
David Duran <dadch1404@gmail.com>
2023-02-28 22:27:19 -0500

    bump: versions

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,29 @@
 module.exports = {
-  content: ["content/**/*.md", "themes/tailwind/layouts/**/*.html"],
+  content: [
+    "content/**/*.md", 
+    "themes/tailwind/layouts/**/*.html",
+    "themes/tailwind/layouts/_default/*.html",
+  ],
   theme: {
     extend: {
       fontFamily: {
         sans: ["Inter var", "sans-serif"],
-      }
+      },
+      colors: {
+        black: "#212b36",
+        dark: "#090E34",
+        "dark-700": "#090e34b3",
+        primary: "#3056D3",
+        secondary: "#13C296",
+        "body-color": "#637381",
+        warning: "#FBBF24",
+      },
+      boxShadow: {
+        input: "0px 7px 20px rgba(0, 0, 0, 0.03)",
+        pricing: "0px 39px 23px -27px rgba(0, 0, 0, 0.04)",
+        "switch-1": "0px 0px 5px rgba(0, 0, 0, 0.15)",
+        testimonial: "0px 60px 120px -20px #EBEFFD",
+      },
     },
   },
   plugins: [],
